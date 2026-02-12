45c38ba4d3780a5f55145205fa546ef77212bbc6
SouthKioto <bolisegabartek6@gmail.com>
2026-02-12 21:09:43 +0100

    rozszerznie palety kolorów dla komentarzy i stringów

--- lua/urahara/init.lua
+++ lua/urahara/init.lua
@@ -6,38 +6,30 @@ function M.setup()
 	local hl = vim.api.nvim_set_hl
 
 	local colors = {
-		-- Bazy
+
 		bg = "#0e100c",
 		bg_alt = "#121510",
 		gray_dark = "#1a1d18",
 		gray_dim = "#4a4f44",
 		near_black = "#22211f",
-
-		-- Zieleń (z palety + rozszerzona)
-		green_faint = "#3a4d30", -- bardzo ciemna, do PreProc / nieaktywnych
-		green_dark = "#323e29", -- #323e29 z palety – separatory, tagi
-		green_mid = "#586d4a", -- #586d4a z palety – keywords, główny akcent
-		green_bright = "#6d8a57", -- jaśniejszy – CursorLineNr, booleans
-		green_soft = "#7a9e68", -- miękki – strings
-		green_vivid = "#92bf7a", -- najjaśniejszy – operators, returns
-
-		-- Beże / kremowe (z palety)
-		cream = "#eeede8", -- #eeede8 – fg, functions
-		parchment = "#e7e3ca", -- #e7e3ca – constants
-		fg_dim = "#c8c5be", -- przyciemniony fg – identifiers
-		fg_muted = "#9a9790", -- jeszcze ciemniejszy – property, variables
-		fg_faint = "#6e6c67", -- najciemniejszy – params, punctuation
-
-		-- Akcenty
-		tan = "#b5a97a", -- złoto-beż – types (wyróżnia się od zieleni)
-		moss = "#4e6b3c", -- mech – delimiters
+		green_faint = "#3a4d30",
+		green_dark = "#323e29",
+		green_mid = "#586d4a",
+		green_bright = "#6d8a57",
+		green_soft = "#7a9e68",
+		green_vivid = "#92bf7a",
+		cream = "#eeede8",
+		parchment = "#e7e3ca",
+		fg_dim = "#c8c5be",
+		fg_muted = "#9a9790",
+		fg_faint = "#6e6c67",
+		tan = "#b5a97a",
+		moss = "#4e6b3c",
 		visual_bg = "#1e2b19",
 	}
-
-	-- Base
 	hl(0, "Normal", { fg = colors.cream, bg = colors.bg })
 	hl(0, "NormalFloat", { fg = colors.cream, bg = colors.bg_alt })
-	hl(0, "Comment", { fg = "#50584a", italic = true })
+	hl(0, "Comment", { fg = "#c8c5be", italic = true })
 	hl(0, "CursorLine", { bg = colors.gray_dark })
 	hl(0, "LineNr", { fg = colors.gray_dim })
 	hl(0, "CursorLineNr", { fg = colors.green_bright, bold = true })
@@ -46,11 +38,9 @@ function M.setup()
 	hl(0, "EndOfBuffer", { fg = colors.green_faint })
 	hl(0, "ColorColumn", { bg = "#161a13" })
 	hl(0, "MatchParen", { fg = colors.green_vivid, bold = true, underline = true })
-
-	-- Syntax – każda kategoria ma wyraźnie inny kolor
 	hl(0, "Keyword", { fg = colors.green_mid, bold = true })
 	hl(0, "Statement", { fg = colors.green_mid, bold = true })
-	hl(0, "String", { fg = colors.green_soft })
+	hl(0, "String", { fg = "#dddbd6" })
 	hl(0, "Function", { fg = colors.cream, bold = true })
 	hl(0, "Identifier", { fg = colors.fg_dim })
 	hl(0, "Operator", { fg = colors.green_vivid })
@@ -61,15 +51,11 @@ function M.setup()
 	hl(0, "Boolean", { fg = colors.green_bright, bold = true })
 	hl(0, "Special", { fg = "#6e9458" })
 	hl(0, "PreProc", { fg = colors.green_faint })
-
-	-- LSP
 	hl(0, "@lsp.type.class", { link = "Type" })
 	hl(0, "@lsp.type.function", { link = "Function" })
 	hl(0, "@lsp.type.method", { link = "Function" })
 	hl(0, "@lsp.type.variable", { link = "@variable" })
 	hl(0, "@lsp.type.parameter", { link = "@variable.parameter" })
-
-	-- Treesitter
 	hl(0, "@punctuation.bracket", { fg = colors.fg_faint })
 	hl(0, "@punctuation.delimiter", { fg = colors.moss })
 	hl(0, "@constructor", { fg = colors.green_bright })
@@ -86,8 +72,6 @@ function M.setup()
 	hl(0, "@tag.delimiter", { fg = colors.green_dark })
 	hl(0, "@string.escape", { fg = colors.tan })
 	hl(0, "@string.special", { fg = colors.tan })
-
-	-- NeoTree
 	hl(0, "NeoTreeNormal", { fg = colors.fg_dim, bg = "NONE" })
 	hl(0, "NeoTreeNormalNC", { fg = colors.fg_dim, bg = "NONE" })
 	hl(0, "NeoTreeWinSeparator", { fg = colors.green_dark })
@@ -95,14 +79,10 @@ function M.setup()
 	hl(0, "NeoTreeDirectoryIcon", { fg = colors.green_mid })
 	hl(0, "NeoTreeFileName", { fg = colors.fg_dim })
 	hl(0, "NeoTreeModified", { fg = colors.tan })
-
-	-- Telescope
 	hl(0, "TelescopeBorder", { fg = colors.green_mid })
 	hl(0, "TelescopePromptBorder", { fg = colors.green_bright })
 	hl(0, "TelescopeSelection", { bg = colors.visual_bg, fg = colors.cream })
 	hl(0, "TelescopeMatching", { fg = colors.green_vivid, bold = true })
-
-	-- UI
 	hl(0, "Visual", { bg = colors.visual_bg })
 	hl(0, "Search", { fg = colors.near_black, bg = colors.green_mid })
 	hl(0, "IncSearch", { fg = colors.near_black, bg = colors.green_vivid })
@@ -113,24 +93,17 @@ function M.setup()
 	hl(0, "PmenuThumb", { bg = colors.green_dark })
 	hl(0, "StatusLine", { fg = colors.fg_dim, bg = colors.near_black })
 	hl(0, "StatusLineNC", { fg = colors.gray_dim, bg = colors.near_black })
-
-	-- Lazy
 	hl(0, "LazyNormal", { bg = "NONE", fg = colors.cream })
 	hl(0, "LazyBorder", { fg = colors.green_mid, bg = "NONE" })
-
-	-- Diagnostics
 	hl(0, "DiagnosticError", { fg = "#c97a5e" })
 	hl(0, "DiagnosticWarn", { fg = colors.tan })
 	hl(0, "DiagnosticInfo", { fg = colors.green_soft })
 	hl(0, "DiagnosticHint", { fg = colors.green_mid })
 	hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "#c97a5e" })
-
-	-- Git signs
 	hl(0, "GitSignsAdd", { fg = colors.green_mid })
 	hl(0, "GitSignsChange", { fg = colors.tan })
 	hl(0, "GitSignsDelete", { fg = "#8a5040" })
 end
-
 M.colors = {
 	bg = "#0e100c",
 	bg_alt = "#121510",
